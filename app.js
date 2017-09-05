96d1a72bb1d1fab24d3ae081b030ea7d4d1b46bd
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-09-05 13:31:04 +0200

    working map with geolocation

---
+++ app.js
@@ -0,0 +1,87 @@
+var map;
+var infowindow;
+
+
+
+function initMap() {
+    var palma = new google.maps.LatLng(39.5696, 2.6502);
+    var test = new google.maps.LatLng(33.5696, 2.6502);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: palma,
+        zoom: 14
+    });
+
+
+    infoWindow = new google.maps.InfoWindow({});
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here');
+            infoWindow.open(map);
+            map.setCenter(pos);
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                icon:'img/person.png'}
+            );
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                marker.setAnimation(null)
+            }, 4000);
+            marker.setMap(map);
+
+
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: pos,
+                radius: 500,
+                type: ['restaurant']
+            }, callback);
+            //service.textSearch(request, callback);
+            service.getDetails(request, callback);
+            function callback(results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        createMarker(results[i]);
+                    }
+                }
+            }
+
+            /*function createMarker(place) {
+                var placeLoc = place.geometry.location;
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: place.geometry.location
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.setContent(place.name);
+                    infowindow.open(map, this);
+                });
+            }*/
+
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
+
+
+
+
+}
+
+
+
